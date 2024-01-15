f4608d16d155bc625f321365fc4ceb6efefee568
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-01-15 22:12:16 +0500

    complete navbar for tab and laptop

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import "./navbar.css";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
-
+// import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import { logoSvg } from "../../assets";
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} alt="logo" />
+          <img src={logoSvg} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#feature">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
         </div>
       </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button>Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu"></div>
     </div>
   );
 };
